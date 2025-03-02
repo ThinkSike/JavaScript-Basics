0d203ab78eee6091d77519dd4ce5fcf05d11b058
ThinkSike <138986417+ThinkSike@users.noreply.github.com>
2025-03-02 10:12:49 +0530

    06_string.js

--- Basics/06_string.js
+++ Basics/06_string.js
@@ -10,3 +10,21 @@ console.log(typeof gameName)
 console.log(gameName.length)
 console.log(gameName.__proto__)
 console.log(gameName.charAt(0))
+console.log(gameName.indexOf('a'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString)
+
+const newString1 = "   too much space..      "
+console.log(newString1)
+console.log(newString1.trim())
+
+const url = "https://in/tiyabhavsar.com/tiya%20bhavsar"
+
+console.log(url.replace('%20', '-'))
+console.log(url.includes('tiya'))
+
+console.log(gameName.split('-'))
