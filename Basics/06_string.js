d7e9706846b5393633767de790e7e257db747aae
ThinkSike <138986417+ThinkSike@users.noreply.github.com>
2025-03-02 09:30:43 +0530

    Add files via upload

---
+++ Basics/06_string.js
@@ -0,0 +1,12 @@
+const name = 'Tiya'
+const repoCount = 90
+
+console.log(name + repoCount + "Value")
+console.log("Heloo my name is ${name} and my repoCount is ${repoCount}")
+
+const gameName = new String('Valorant')
+console.log(gameName)
+console.log(typeof gameName)
+console.log(gameName.length)
+console.log(gameName.__proto__)
+console.log(gameName.charAt(0))
